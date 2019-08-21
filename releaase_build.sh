f950b64d327645b48ff14e73cbd219f3d3645df6
candoizo <candoizo@protonmail.com>
2019-08-21 12:59:38 -0400

    feat: changes for arm64e go

--- releaase_build.sh
+++ releaase_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export ARCHS=arm64 # arm64e
+export ARCHS=arm64 arm64e
 export FINALPACKAGE=1
-export THEOS_DEVICE_IP=192.168.12.207
+export THEOS_DEVICE_IP=192.168.12.233
 # export THEOS_DEVICE_IP=172.20.10.2
 export THEOS_PACKAGE_DIR=/home/candoizo/Code/ios/2019/masqkit/public
 cd src
